9b8dd943c6e227d1c55492e0bc5d310cbf30c670
joel543-sv <156801264+joel543-sv@users.noreply.github.com>
2025-06-17 11:48:16 -0300

    proyecto listo

---
+++ main (1).py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Sistema de Gestión para una Clínica
+Universidad de Mendoza - Computación I - 2025
+
+Punto de entrada principal del sistema.
+"""
+
+from src.cli.cli import main
+
+if __name__ == "__main__":
+    main()
